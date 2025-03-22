cc42de52ab9c79c6d2929796e6ad3c4256bf6415
SnurfDev <jakob.kruse08@gmail.com>
2025-03-22 17:33:44 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,127 @@
+# streampack
+
+Easily read and write values to streams and buffers.
+This module is a easier to use and typescript compatible version of the [binary](https://www.npmjs.com/package/binary) module that also works for writing.
+
+## Usage
+
+The package implements two main classes:
+
+- `StreamReader` for reading from streams / buffers.
+- `StreamWriter` for writing to streams / buffers.
+
+In the initializer you need to specify a source/destination buffer or stream (Extends `Readable` / `Writable`). You can also optionally specify that the stream is big endian as the second argument (little endian is used by default).
+
+### Types
+
+The functions for reading and writing numbers are named after the rust number types as I find them easy to use.
+
+- `i32`: 32 bit signed integer
+- `u64`: 64 bit unsigned integer
+- `f32`: 32 bit float
+- `u8`: byte
+
+Then there is a `bool` function which is just a byte (`0x00` = false, everything else is true).
+
+For strings there are 3 functions:
+
+- `c_str`: a string with null termination
+- `w_str`: a wide (16 bit) string with null termination
+- `n_str`: a string prefixed by its length as a singular byte
+
+For general reading and writing of raw buffers there is a `bytes` function which takes the length as an argument.
+
+### Dynamic Buffer Size
+
+When writing to a buffer you normally need to account for the maximum size when allocating it. If you want to use a dynamic buffer size however, you can use the built in `BufferWriter` class like this:
+
+```typescript
+import { BufferWriter, StreamWriter } from "streampack"
+const bufferWriter = new BufferWriter(
+    Buffer.alloc(0) // Empty Buffer
+    true            // Allow Resizing
+);
+
+const writer = new StreamWriter(bufferWriter);
+writer.u8(1)
+writer.u8(2)
+writer.u8(3)
+
+console.log(bufferWriter.buffer) // <Buffer 01 02 03>
+```
+
+## Examples
+
+### Reading
+
+You can read from either a stream or a buffer, this script reads two 32 bit integers (one signed and one unsigned), as well as a 64 bit float from stdin:
+
+```typescript
+import {StreamReader} from "streampack";
+
+const reader = new StreamReader(process.stdin);
+
+console.log("Value 1: " + reader.i32());
+console.log("Value 2: " + reader.u32());
+console.log("Value 3: " + reader.f64());
+```
+
+This script does the same for a buffer but this time with **big endianness**:
+
+```typescript
+import {StreamReader} from "streampack";
+
+const buffer = Buffer.from([
+    0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x02, 0x40, 0x08, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+]);
+
+const reader = new StreamReader(
+    process.stdin,
+    true, // big endian
+);
+
+console.log("Value 1: " + reader.i32()); // 1
+console.log("Value 2: " + reader.u32()); // 2
+console.log("Value 3: " + reader.f64()); // 3
+```
+
+### Writing
+
+Using this same method you can also write to buffers / streams.
+This script writes a null terminated string (cstring) and the number 31 as a uint32 to stdout:
+
+```typescript
+import {StreamWriter} from "streampack";
+
+const writer = new StreamWriter(process.stdout);
+
+writer.c_str("Hello World!");
+writer.u32(31);
+```
+
+This writes a length prefixed string to a buffer:
+
+```typescript
+import {StreamWriter} from "streampack";
+
+const buf = Buffer.alloc(32);
+
+const writer = new StreamWriter(process.stdout);
+
+writer.n_str("Hello World!");
+
+console.log(buf); // <Buffer 0c 48 65 6c 6c 6f 20 57 6f 72 6c 64 21 00 ...
+```
+
+# Installation
+
+To install with npm:
+
+```bash
+npm i streampack
+```
+
+# License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,295 @@
+import {Readable, Writable} from "node:stream";
+
+export class BufferWriter extends Writable {
+    private ptr = 0;
+
+    constructor(
+        public buffer: Buffer = Buffer.alloc(0),
+        private allowResize = true,
+    ) {
+        super();
+    }
+
+    _write(
+        chunk: any,
+        encoding: BufferEncoding,
+        callback: (error?: Error | null) => void,
+    ): void {
+        if (!(chunk instanceof Buffer) && typeof chunk !== "string")
+            return callback(new Error("Invalid chunk type: " + typeof chunk));
+        let data: Buffer;
+
+        if (typeof chunk == "string") {
+            data = Buffer.from(chunk, encoding);
+        } else {
+            data = chunk;
+        }
+
+        if (this.ptr + data.length > this.buffer.length) {
+            if (!this.allowResize)
+                return callback(new Error("Write out of bounds of buffer"));
+
+            this.buffer = Buffer.concat([
+                this.buffer,
+                Buffer.alloc(this.ptr + chunk.length - this.buffer.length),
+            ]);
+        }
+
+        this.ptr += data.copy(this.buffer, this.ptr);
+    }
+}
+
+export interface ReadableStruct {
+    read(reader: StreamReader): void;
+}
+
+export class StreamReader {
+    stream: Readable;
+    private chunk: Buffer = Buffer.alloc(0);
+
+    private get endiannessSuffix() {
+        return this.isBigEndian ? "BE" : "LE";
+    }
+
+    constructor(
+        source: Buffer | Readable,
+        private isBigEndian: boolean = false,
+    ) {
+        if (source instanceof Buffer) {
+            this.stream = Readable.from(source);
+        } else {
+            this.stream = source as Readable;
+        }
+    }
+
+    private read(n: number): Buffer {
+        let ndata = this.stream.read();
+
+        if (ndata) this.chunk = Buffer.concat([this.chunk, ndata]);
+
+        if (this.chunk.length < n)
+            throw new Error("Stream can't keep up with reader.");
+
+        let buf = this.chunk.subarray(0, n);
+
+        this.chunk = this.chunk.subarray(n);
+
+        return buf;
+    }
+
+    // byte
+    u8() {
+        return this.read(1)["readInt8"]();
+    }
+
+    // integers
+    u16() {
+        return this.read(2)[`readUInt16${this.endiannessSuffix}`]();
+    }
+    u32() {
+        return this.read(4)[`readUInt32${this.endiannessSuffix}`]();
+    }
+    u64() {
+        return this.read(4)[`readBigUInt64${this.endiannessSuffix}`]();
+    }
+    i16() {
+        return this.read(2)[`readInt16${this.endiannessSuffix}`]();
+    }
+    i32() {
+        return this.read(4)[`readInt32${this.endiannessSuffix}`]();
+    }
+    i64() {
+        return this.read(4)[`readBigInt64${this.endiannessSuffix}`]();
+    }
+
+    // floats
+    f32() {
+        return this.read(4)[`readFloat${this.endiannessSuffix}`]();
+    }
+    f64() {
+        return this.read(4)[`readDouble${this.endiannessSuffix}`]();
+    }
+
+    // boolean
+    bool() {
+        return this.u8() != 0;
+    }
+
+    // c-string
+    c_str() {
+        let str = "";
+        let b: Buffer;
+
+        while ((b = this.read(1))[0]) {
+            str += b.toString("utf8");
+        }
+
+        return str;
+    }
+
+    c_wstr() {
+        let str = "";
+        let b: Buffer;
+
+        while ((b = this.read(2))[`readUint16${this.endiannessSuffix}`]()) {
+            str += b.toString("utf16le");
+        }
+
+        return str;
+    }
+
+    // n-string
+    n_str() {
+        return this.read(this.u8()).toString("utf8");
+    }
+
+    // raw
+    bytes(n: number) {
+        return this.read(n);
+    }
+
+    // struct
+    struct<T extends ReadableStruct>(struct: new () => T): T {
+        const v = new struct();
+        v.read(this);
+        return v;
+    }
+}
+
+function bufferWriter<
+    T extends {
+        [K in keyof Buffer]: K extends `write${string}` ? K : never;
+    }[keyof Buffer],
+>(fn: T, v: Parameters<Buffer[T]>[0]): Buffer {
+    const buf = Buffer.allocUnsafe(256); // Max Len
+    const n = (buf[fn] as (v: Parameters<Buffer[T]>[0]) => number)(v);
+    return buf.subarray(0, n);
+}
+
+export interface WritableStruct {
+    write(writer: StreamWriter): void;
+}
+
+export class StreamWriter {
+    stream: Writable;
+
+    private get endiannessSuffix() {
+        return this.isBigEndian ? "BE" : "LE";
+    }
+
+    constructor(
+        destination: Writable,
+        private isBigEndian: boolean = false,
+    ) {
+        this.stream = destination;
+    }
+
+    private write(bytes: Buffer): void {
+        this.stream.write(bytes);
+    }
+
+    // byte
+    u8(value: number) {
+        this.write(bufferWriter("writeInt8", value));
+        return this;
+    }
+
+    // integers
+    u16(value: number) {
+        this.write(bufferWriter(`writeUInt16${this.endiannessSuffix}`, value));
+        return this;
+    }
+    u32(value: number) {
+        this.write(bufferWriter(`writeUInt32${this.endiannessSuffix}`, value));
+        return this;
+    }
+    u64(value: bigint) {
+        this.write(
+            bufferWriter(`writeBigUInt64${this.endiannessSuffix}`, value),
+        );
+        return this;
+    }
+    i16(value: number) {
+        this.write(bufferWriter(`writeInt16${this.endiannessSuffix}`, value));
+        return this;
+    }
+    i32(value: number) {
+        this.write(bufferWriter(`writeInt32${this.endiannessSuffix}`, value));
+        return this;
+    }
+    i64(value: bigint) {
+        this.write(
+            bufferWriter(`writeBigInt64${this.endiannessSuffix}`, value),
+        );
+        return this;
+    }
+
+    // floats
+    f32(value: number) {
+        this.write(bufferWriter(`writeFloat${this.endiannessSuffix}`, value));
+        return this;
+    }
+    f64(value: number) {
+        this.write(bufferWriter(`writeDouble${this.endiannessSuffix}`, value));
+        return this;
+    }
+
+    // boolean
+    bool(value: boolean) {
+        this.u8(value ? 1 : 0);
+    }
+
+    // c-string
+    c_str(str: string) {
+        this.write(Buffer.from(str + "\x00", "utf8"));
+        return this;
+    }
+
+    c_wstr(str: string) {
+        this.write(Buffer.from(str + "\x00", "utf16le"));
+        return this;
+    }
+
+    // n-string
+    n_str(str: string) {
+        this.u8(str.length);
+        this.write(Buffer.from(str, "utf8"));
+        return this;
+    }
+
+    // raw
+    bytes(bytes: Buffer) {
+        this.write(bytes);
+        return this;
+    }
+
+    // struct
+    struct<T extends WritableStruct>(struct: new () => T) {
+        const v = new struct();
+        v.write(this);
+        return this;
+    }
+}
+
+export function readFlags<const F extends readonly string[]>(
+    buf: Buffer,
+    a: F,
+): {[I in F[number]]: boolean} {
+    return Object.fromEntries(
+        a.map((k, i) => [k, ((buf[Math.floor(i / 8)] >> i % 8) & 1) != 0]),
+    ) as {[I in F[number]]: boolean};
+}
+
+export function writeFlags(flags: {[k: string]: boolean}): Buffer {
+    const keys = Object.keys(flags);
+    const buf = Buffer.alloc(Math.ceil(keys.length / 8));
+    for (let i = 0; i < Math.ceil(keys.length / 8); i++) {
+        let value = 0;
+        for (let h = 0; h < Math.min(8, keys.length - i * 8); h++) {
+            value |= (flags[keys[i * 8 + h]] ? 1 : 0) << h;
+        }
+    }
+    return buf;
+}
+
+export type RWStruct = ReadableStruct & WritableStruct;
